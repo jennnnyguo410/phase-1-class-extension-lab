d13b74c943567ebd40e219d26a299797c8b29654
Hao Guo <jennnnyguo@gmail.com>
2023-06-29 11:20:38 +1000

    finish Polygon method

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Your code here
 class Polygon {
+    constructor(sides) {
+        this.sides = sides
+    }
 
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce((a, b) => a + b, 0)
+    }
 }
+
